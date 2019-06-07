5963215f7ea7089bd31c6393c8eb9ec367a0057f
Thomas Wirth <thomas@wtho.de>
2019-06-08 01:06:30 +0200

    feat: add jest, remove karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+
+
+  beforeEach(() => {
+    return TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
